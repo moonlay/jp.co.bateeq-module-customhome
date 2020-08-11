89d66fde5338e22f52a3710d7a54663da7629b2a
Muhamad Fajri <hurifajri@gmail.com>
2020-08-11 14:30:10 +0700

    Reorder address composition in account setting and checkout page

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -6,7 +6,11 @@
 var config = {
   map: {
       '*': {
-          changeEmailPassword: 'Moonlay_CustomHome/js/change-email-password'
+          changeEmailPassword: 'Moonlay_CustomHome/js/change-email-password',
+          'Magento_Checkout/template/shipping-address/address-renderer/default.html':
+            'Moonlay_CustomHome/template/shipping-address/address-renderer/default.html',
+          'Magento_Checkout/template/shipping-information/address-renderer/default.html':
+            'Moonlay_CustomHome/template/shipping-information/address-renderer/default.html',
       }
   }
 };
